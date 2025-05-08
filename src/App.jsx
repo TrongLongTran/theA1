13b483ea8e743b1ae60547377bc5a76d334012eb
ttran101@mylangara.ca <09longtr09@gmail.com>
2025-05-07 17:46:54 -0700

    change App data for assignment 1

--- src/App.jsx
+++ src/App.jsx
@@ -8,27 +8,46 @@ function App() {
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-      <p>{"95190d2 (HEAD -> master) Initial commit: set up Vite React app"}</p>
+    <h1>Lab 1: Git + Vite Setup</h1>
+
+    <h3>Task 2: Git Config</h3>
+    <ul>
+      <li>Running confid --global user.name/email will set name and email for our repositories</li>
+      <li>It will be inside user.name</li>
+      <li>git config --list will return
+        <img src='../../ex3.png'></img>
+      </li>
+      <li>We use "git status" to check if git is initialized or not</li>
+    </ul>
+
+    <h3>Task 3: Commit Messages & Log</h3>
+    <ul>
+      <li>We need to write commit messages in present tense because. It's easier for other users and developers to know what have changed, show what the commit does</li>
+      <li>{"95190d2 (HEAD -> master) Initial commit: set up Vite React app"}</li>
+    </ul>
+
+    <h3>Task 4: git diff</h3>
+    <p>Git diff show the changes between staging area and directory, see changes line by line</p>
+    <p>+: mean something are added into the current repository
+       -: means that something are deleted from the repository
+    </p>
+
+    <h3>Task 5: git restore</h3>
+    <p>Git restore: this will restore file from previous commit or list</p>
+
+    <h3>Task 6: git push -u</h3>
+    <p>-u will set upstream and will help to push or pull without going into main repeatedly.</p>
+    <p>in origin main, it will track the relationship between origin and local branch</p>
+
+    <h3>Task 7: Screen of git log</h3>
+    <img src="../../data.png" alt='newEx'></img>
+
+    <h3>.gitignore</h3>
+    <p>.gitignore will tell Git that some files not tracked by it stay untracked</p>
+    <ul>
+      <li>node_modules</li>
+      <li>*.log</li>
+    </ul>
     </>
   )
 }
